f6f04c2c29968fbccce72dfd7e789916c07625a4
hanappe <hanappe@csl.sony.fr>
2019-08-20 16:10:42 +0200

    log partially public

---
+++ src/log_priv.h
@@ -0,0 +1,18 @@
+
+#include "rcom/log.h"
+
+#ifndef _RCOM_LOG_PRIV_H_
+#define _RCOM_LOG_PRIV_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int log_init();
+void log_cleanup();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _RCOM_LOG_PRIV_H_
