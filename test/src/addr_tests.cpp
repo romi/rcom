271596f28e1a41a4dc225f5ed9cb9f65cedc82ef
Doug Boari <doug@tastymushroom.com>
2020-04-21 14:33:52 +0200

    Initial test added to rcom.

---
+++ test/src/addr_tests.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "fff.h"
+}
+
+DEFINE_FFF_GLOBALS;
+
+// Need to include r.h here to make sure when we mock them below they are mocked in the link with the tests.
+#include "r.h"
+#include "addr.h"
+
+FAKE_VALUE_FUNC(void *, safe_malloc, size_t, int);
+FAKE_VOID_FUNC(safe_free, void *);
+FAKE_VOID_FUNC_VARARG(r_err, const char*, ...);
+
+class addr_tests : public ::testing::Test
+{
+protected:
+	addr_tests() = default;
+
+	~addr_tests() override = default;
+
+	void SetUp() override
+    {
+        RESET_FAKE(safe_malloc);
+        RESET_FAKE(safe_free);
+        RESET_FAKE(r_err);
+	}
+
+	void TearDown() override
+    {
+	}
+
+};
+
+TEST_F(addr_tests, new_addr_when_r_new_fails_logs_returns_NULL)
+{
+    // Arrange
+    safe_malloc_fake.return_val = nullptr;
+
+    // Act
+    addr_t *paddr = new_addr0();
+
+    //Assert
+    ASSERT_EQ(paddr, nullptr);
+}
