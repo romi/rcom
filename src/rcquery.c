9231c7bd7aec0d8f2721f686d43c44d4978dee2e
hanappe <hanappe@csl.sony.fr>
2020-07-12 11:12:15 +0200

    add send command

--- src/rcquery.c
+++ src/rcquery.c
@@ -289,7 +289,7 @@ static void print_usage_restart()
 static void print_usage()
 {
         printf("Usage: rcom command [options]\n");
-        printf("  Available commands: list, listen, request, show, stream, help, restart\n");
+        printf("  Available commands: list, listen, send, request, show, stream, help, restart\n");
 }
 
 int main(int argc, char **argv)
