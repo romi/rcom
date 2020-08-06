89f6a459a0be31268fb587adc31884453a3667b1
Doug Boari <doug@tastymushroom.com>
2020-08-06 15:52:37 +0200

    Add python code to run an array of tests. Add logging of websocket data to constructor. Add exception handling to test code.

--- test/integration_test/integration_test.py
+++ test/integration_test/integration_test.py
@@ -1,15 +1,57 @@
 import random
 import registry
 import sys
+import traceback
 
 test_return_value = 1
+OK = "OK"
+Unknown_Request = "Unknown request"
+test_array = None
 
-registry_tests = registry.RegistryTests()
+# Construct registry tests with logging value
+registry_tests = registry.RegistryTests(False)
 
-connection_port = random.randrange(2000, 65535)
-if registry_tests.registry_run(connection_port) is True:
-    if registry_tests.registry_connect() is True:
-        test_return_value = registry_tests.registry_send_test()
 
-registry_tests.registry_terminate()
-sys.exit(test_return_value)
+# Array Format:
+#       Function,       send_data,      success_return_value,       message_string
+def add_tests():
+    global test_array
+    test_array = [
+        (registry_tests.registry_send_test, "{'request':'xxxx'}", False, Unknown_Request),
+        (registry_tests.registry_send_test, "{'request':'list'}", True,  OK),
+        (registry_tests.registry_send_test, "{'request':'xxxx'}", False, Unknown_Request),
+        (registry_tests.registry_send_test, "{'request':'list'}", True,  OK),
+        # Following test will fail.
+        # (registry_tests.registry_send_test, "{'request':'xxxx'}", False, "FAILED"),
+    ]
+
+
+def run_tests():
+    global test_array
+    for test in test_array:
+        function_name, send_data, success_value, message_value = test
+        result = function_name(send_data, success_value, message_value)
+        if result != 0:
+            return 1
+    return 0
+
+
+def main():
+    global test_return_value
+    add_tests()
+    try:
+        connection_port = random.randrange(2000, 65535)
+        if registry_tests.registry_run(connection_port) is True:
+            if registry_tests.registry_connect() is True:
+                test_return_value = run_tests()
+    except Exception:
+        traceback.print_exc()
+    finally:
+        registry_tests.registry_terminate()
+        sys.exit(test_return_value)
+
+
+if __name__ == "__main__":
+    main()
+
+
--- test/integration_test/registry.py
+++ test/integration_test/registry.py
@@ -8,21 +8,23 @@ import ssl
 import logging
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-class RegistryTests:
-    ws = None;
-    websocket.enableTrace(True)
 
+class RegistryTests:
+    ws = None
     connection_port = None
     rcreg = None
+
+    def __init__(self, enable_socket_logging):
+        websocket.enableTrace(enable_socket_logging)
 
     def registry_run(self, connection_port):
         self.connection_port = connection_port
         recregistry_switches = "-P"
         current_path = pathlib.Path(__file__).parent.absolute()
         rcreg_exe = current_path / "rcregistry"
-        print("%s %s %s".format(str(rcreg_exe), recregistry_switches, str(connection_port)))
+        print("RUN COMMAND: " + str(rcreg_exe) + " " + recregistry_switches + " " + str(connection_port))
         self.rcreg = subprocess.Popen([str(rcreg_exe), recregistry_switches, str(connection_port)])
-        time.sleep(2)
+        time.sleep(1)
         if self.rcreg is not None:
             return True
         return False
@@ -37,24 +39,33 @@ class RegistryTests:
         print("connecting to: " + "ws://127.0.0.1:"+str(self.connection_port))
         self.ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
         self.ws.connect("ws://127.0.0.1:"+str(self.connection_port))
-        # self.ws = websocket.create_connection("ws://127.0.0.1:"+str(self.connection_port))
         return self.ws.connected
 
-    def registry_send_test(self):
-        return_code = 0
-        print("Sending {'request':'list'}")
-        self.ws.send("{'request':'list'}")
-        print("Sent")
-        print("Receiving...")
-        result = self.ws.recv()
-        self.ws.close()
-        print("Received '%s'" % result)
-        print("Done\r\n")
-
+    def registry_check_results(self, result, success_response, message_response):
+        test_results = False
         result_dictionary = json.loads(result)
-        if result_dictionary["success"]:
-            print("success")
+        if result_dictionary["success"] != success_response:
+            print("RESULT: '%s'" % result)
+            print("TEST ERROR: success != " + "\"" + success_response + "\"")
+        elif result_dictionary["message"] != message_response:
+            print("RESULT: '%s'" % result)
+            print("Test ERROR: message != " + "\"" + message_response + "\"")
         else:
-            print("failed")
+            test_results = True
+        return test_results
+
+    def registry_send_test(self, send_data, success_response, message_response):
+        return_code = 0
+        print("TEST: registry_send_test \"" + send_data + "\" \"" + str(success_response) + "\" \"" + message_response + "\"")
+        self.ws.send(send_data)
+        result = self.ws.recv()
+        # print("RESULT: '%s'" % result)
+        if self.registry_check_results(result, success_response, message_response):
+            print("TEST SUCCESS")
+        else:
+            print("TEST FAILED")
             return_code = 1
         return return_code
+
+
+
