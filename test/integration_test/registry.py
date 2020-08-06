3352e75960e8526a6e19e4a9fb4d7f55b95b59e4
Doug Boari <doug@tastymushroom.com>
2020-08-06 10:46:30 +0200

    Refactor python websocket connection.

--- test/integration_test/registry.py
+++ test/integration_test/registry.py
@@ -1,35 +1,44 @@
-from websocket import create_connection
+import websocket
 import json
 import subprocess
 import time
 import pathlib
+import sys
+import ssl
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 class RegistryTests:
-    ws = None
+    ws = None;
+    # websocket.enableTrace(True)
+
     connection_port = None
     rcreg = None
 
     def registry_run(self, connection_port):
         self.connection_port = connection_port
+        recregistry_switches = "-P"
         current_path = pathlib.Path(__file__).parent.absolute()
         rcreg_exe = current_path / "rcregistry"
-        print("rcreg_exe '%s'" % rcreg_exe)
-        self.rcreg = subprocess.Popen([str(rcreg_exe), "-P", str(connection_port)])
+        print("%s %s %s".format(str(rcreg_exe), recregistry_switches, str(connection_port)))
+        self.rcreg = subprocess.Popen([str(rcreg_exe), recregistry_switches, str(connection_port)])
         time.sleep(2)
         if self.rcreg is not None:
             return True
         return False
 
     def registry_terminate(self):
+        if self.ws.connected:
+            self.ws.close(timeout=3)
         if self.rcreg is not None:
             self.rcreg.terminate()
 
     def registry_connect(self):
         print("connecting to: " + "ws://127.0.0.1:"+str(self.connection_port))
-        self.ws = create_connection("ws://127.0.0.1:"+str(self.connection_port))
-        if self.rcreg is not None:
-            return True
-        return False
+        self.ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+        self.ws.connect("ws://127.0.0.1:"+str(self.connection_port))
+        # self.ws = websocket.create_connection("ws://127.0.0.1:"+str(self.connection_port))
+        return self.ws.connected
 
     def registry_send_test(self):
         return_code = 0
