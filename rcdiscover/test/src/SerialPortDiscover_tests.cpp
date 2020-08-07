46d76c8fe99e1ca68b01ad53bded8f195656c5aa
Doug Boari <doug@tastymushroom.com>
2020-08-07 15:07:54 +0200

    Add basic synchronisation to serial port test thread.

--- rcdiscover/test/src/SerialPortDiscover_tests.cpp
+++ rcdiscover/test/src/SerialPortDiscover_tests.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <future>
+#include <thread>
 
 #include <r.h>
 
@@ -39,6 +40,7 @@ protected:
     }
 
 protected:
+    std::vector<std::string> knownDevices = {"Grbl", "BrushMotorController", "RomiControlPanel"};
 
 };
 
@@ -47,13 +49,13 @@ TEST_F(SerialPortDiscover_tests, SerialPortDiscover_can_construct)
     // Arrange
     // Act
     //Assert
-    ASSERT_NO_THROW(SerialPortDiscover SerialPortDiscover);
+    ASSERT_NO_THROW(SerialPortDiscover SerialPortDiscover(knownDevices));
 }
 
 TEST_F(SerialPortDiscover_tests, SerialPortDiscover_ConnectedDevice_bad_port_returns_empty_string)
 {
     // Arrange
-    SerialPortDiscover SerialPortDiscover;
+    SerialPortDiscover SerialPortDiscover(knownDevices);
     const int32_t timeout_ms = 100;
     // Act
     auto device = SerialPortDiscover.ConnectedDevice(std::string("/dev/notreal"), timeout_ms);
@@ -65,7 +67,7 @@ TEST_F(SerialPortDiscover_tests, SerialPortDiscover_ConnectedDevice_bad_port_ret
 TEST_F(SerialPortDiscover_tests, SerialPortDiscover_ConnectedDevice_times_out_returns_empty_string)
 {
     // Arrange
-    SerialPortDiscover SerialPortDiscover;
+    SerialPortDiscover SerialPortDiscover(knownDevices);
     std::string port0 = CppLinuxSerial::TestUtil::GetInstance().GetDevice0Name();
     const int32_t timeout_ms = 100;
     // Act
@@ -75,14 +77,15 @@ TEST_F(SerialPortDiscover_tests, SerialPortDiscover_ConnectedDevice_times_out_re
     ASSERT_TRUE(device.empty());
 }
 
-int ThreadFunction(const std::string& port, const std::string& controller)
+int ThreadFunction(const std::string& port, const std::string& controller, std::condition_variable *cv)
 {
+
     const int bufflen = 256;
     int result = -1;
     char buffer[bufflen];
     memset(buffer, 0, bufflen);
-
     serial_t * serial_port = new_serial(port.c_str(), 115200, 0);
+    cv->notify_one();
     if ( serial_port )
     {
         if (serial_read_timeout(serial_port, buffer, bufflen, 1000) == 0)
@@ -104,18 +107,27 @@ int ThreadFunction(const std::string& port, const std::string& controller)
 TEST_F(SerialPortDiscover_tests, SerialPortDiscover_ConnectedDevice_returns_connected_device)
 {
     // Arrange
-    std::string controller("brushmotorcontroller\n");
-
+    const int timeout_ms = 500;
+    std::mutex mutex;
+    std::unique_lock<std::mutex> lk(mutex);
+    std::condition_variable cv;
+    std::string actual_device;
+    std::string expected_device(knownDevices[0]);
     std::string port0 = CppLinuxSerial::TestUtil::GetInstance().GetDevice0Name();
     std::string port1 = CppLinuxSerial::TestUtil::GetInstance().GetDevice1Name();
 
-    auto future = std::async(ThreadFunction, port1, controller);
-    SerialPortDiscover SerialPortDiscover;
-    const int timeout_ms = 500;
+    SerialPortDiscover SerialPortDiscover(knownDevices);
+    // NOTE: DON'T FORGET \n its canonical!
+    auto future = std::async(ThreadFunction, port1, expected_device+"\n", &cv);
+
     // Act
-    auto device = SerialPortDiscover.ConnectedDevice(port0, timeout_ms);
+    if (cv.wait_for(lk, std::chrono::milliseconds(timeout_ms)) != std::cv_status::timeout)
+    {
+        actual_device = SerialPortDiscover.ConnectedDevice(port0, timeout_ms);
+    }
+    int thread_success = future.get();
 
     // Assert
-    ASSERT_EQ(controller, device);
-
+    ASSERT_EQ(expected_device, actual_device);
+    ASSERT_EQ(thread_success, 0);
 }
